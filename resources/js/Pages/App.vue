5959b79b352ddf6176a5db24d850637ebfcba2fa
Dickson <kibedickson3@gmail.com>
2022-02-02 15:32:41 +0300

    Initial Commit

---
+++ resources/js/Pages/App.vue
@@ -0,0 +1,23 @@
+<template>
+    <Nav></Nav>
+    <main>
+        <slot></slot>
+    </main>
+</template>
+<script>
+import Nav from '../Components/Nav.vue'
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+    components: {
+        Nav
+    },
+    setup() {
+
+        return {
+
+        }
+    }
+})
+
+</script>
---
+++ resources/js/Pages/Movies/index.vue
@@ -0,0 +1,23 @@
+<template>
+    <app-layout>
+        <h1>Hello World</h1>
+    </app-layout>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+import AppLayout from '../App'
+
+  export default defineComponent({
+    components: {
+        AppLayout
+    },
+
+    setup(){
+
+        return {
+
+        }
+    }
+  })
+</script>
