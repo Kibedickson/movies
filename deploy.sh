488aaca41c264f81f9037e00df914ab7f2eeee28
Dickson <kibedickson3@gmail.com>
2022-02-16 16:56:37 +0300

    Build for Prod

--- deploy.sh
+++ deploy.sh
@@ -1,17 +1,17 @@
 #!/bin/sh
 set -e
 
-git push
-
-git checkout production
-git merge main -m "Merged main into production"
-
 #npm build for production
 npm run prod
 
 git add .
 
 git commit -m "Build for Prod"
+
+git push
+
+git checkout production
+git merge main -m "Merged main into production"
 
 git push origin production
 
